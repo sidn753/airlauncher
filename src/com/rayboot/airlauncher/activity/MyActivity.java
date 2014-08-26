c065208bcc48652e273a58912e3a3032c3e1c6e6
rayboot <rayboot.work@gmail.com>
2014-08-26 18:07:41 +0800

    fix 完善

--- src/com/rayboot/airlauncher/activity/MyActivity.java
+++ src/com/rayboot/airlauncher/activity/MyActivity.java
@@ -3,23 +3,37 @@ package com.rayboot.airlauncher.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import com.rayboot.airlauncher.R;
-import com.rayboot.airlauncher.base.BaseActionBarActivity;
+import com.rayboot.airlauncher.base.BaseActivity;
 
-public class MyActivity extends BaseActionBarActivity
+public class MyActivity extends BaseActivity
 {
+    @InjectView(R.id.imageview) ImageView mImageview;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+        setTheme(android.R.style.Theme_NoTitleBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        startActivity(new Intent(this, HomeActivity.class));
+        ButterKnife.inject(this);
+
+        mImageview.postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                startActivity(new Intent(MyActivity.this, HomeActivity.class));
+            }
+        }, 3000);
     }
 
     public void onEvent(View view)
     {
 
     }
-
 }
